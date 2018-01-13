8a5441ff5bdb018b886535b2f699e8328bf83ee4
Ian Schreuder <ian.schreuder@gmail.com>
2018-01-13 14:28:42 -0700

    add selllimit & cancel order

--- lib/bittrex_rb/endpoints/market.rb
+++ lib/bittrex_rb/endpoints/market.rb
@@ -11,6 +11,22 @@ module BittrexRb
         ::BittrexRb::Response::Market.new(res)
       end
 
+      # parameter - required - description
+      # market - required - a string literal for the market (ex: BTC-LTC)
+      # quantity - required - the amount to purchase
+      # rate - required - the rate at which to place the order
+      def selllimit(pair, quantity, rate)
+        res = sget("/selllimit", {market: pair, quantity: quantity, rate: rate})
+        ::BittrexRb::Response::Market.new(res)
+      end
+
+      # parameter - required - description
+      # uuid - required - uuid of buy or sell order
+      def cancelorder(uuid)
+        res = sget("/cancelorder", {uuid: uuid})
+        ::BittrexRb::Response::Market.new(res)
+      end
+
       def uri_modifier; '/market'; end
 
     end
