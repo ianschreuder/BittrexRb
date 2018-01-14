6e0810090da49db204cc3f62fdb56f633b3ec01f
Ian Schreuder <ian.schreuder@gmail.com>
2018-01-13 22:15:44 -0700

    better response handling

--- lib/bittrex_rb/response/public.rb
+++ lib/bittrex_rb/response/public.rb
@@ -3,14 +3,31 @@ module BittrexRb
     class Public < Base
 
       def bids
-        a = body["result"] || {}
-        to_hashes(a["buy"])
+        a = body["result"]
+        if a.is_a?(Hash)
+          to_hashes(a["buy"])
+
+        elsif a.is_a?(Array)
+          to_hashes(a)
+
+        else
+          []
+
+        end
       end
 
       def asks
-        a = body["result"] || {}
-        to_hashes(a["sell"])
+        a = body["result"]
+        if a.is_a?(Hash)
+          to_hashes(a["sell"])
 
+        elsif a.is_a?(Array)
+          to_hashes(a)
+
+        else
+          []
+
+        end
       end
 
       private
